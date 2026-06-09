96a95e35374c9b1b9179596d22e88e07ed149ae4
Usuario01-bit <calitovr6@gmail.com>
2026-06-08 21:46:42 -0500

    feat: enable Firestore offline persistence for offline sales
    Configure persistenceEnabled and CACHE_SIZE_UNLIMITED in main()
    so sales can be created without internet and sync when back online.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -204,6 +205,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.settings = Settings(
+    persistenceEnabled: true,
+    cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+  );
 
   try {
     await FirestoreService.instance.seedFromLocal();

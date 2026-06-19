7f27f276a725030df84b473a21bb68aae1aeda16
Usuario01-bit <calitovr6@gmail.com>
2026-06-19 11:31:29 -0500

    fix: add confirm=yes to Drive download URL

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -3,5 +3,5 @@ class Env {
 
   static const supabaseUrl = 'https://zplmmgojrocmiummiqsu.supabase.co';
   static const supabaseAnonKey = 'sb_publishable_N_aED3l_U8IbyZ8J57zcwA_JZeT21xc';
-  static const downloadUrl = 'https://drive.google.com/uc?export=download&id=1QvXWy4f9gFIqDPoJaKxEu0FhBczRV9rW';
+  static const downloadUrl = 'https://drive.google.com/uc?export=download&confirm=yes&id=1QvXWy4f9gFIqDPoJaKxEu0FhBczRV9rW';
 }

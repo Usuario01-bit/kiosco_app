e551cd649034b46c9085de0b1c8cc21a2f68daee
Usuario01-bit <calitovr6@gmail.com>
2026-06-30 13:20:26 -0500

    feat: update product icons mapper to Lucide

--- lib/services/product_icons.dart
+++ lib/services/product_icons.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 const Map<String, IconData> productIcons = {
-  'inventory_2': Icons.inventory_2_outlined,
-  'icecream': Icons.icecream_outlined,
-  'local_pizza': Icons.local_pizza_outlined,
-  'lunch_dining': Icons.lunch_dining_outlined,
-  'local_drink': Icons.local_drink_outlined,
-  'water_drop': Icons.water_drop_outlined,
-  'breakfast_dining': Icons.breakfast_dining_outlined,
-  'donut_small': Icons.donut_small_outlined,
-  'cookie': Icons.cookie_outlined,
-  'apple': Icons.apple_outlined,
-  'fastfood': Icons.fastfood_outlined,
-  'cake': Icons.cake_outlined,
-  'coffee': Icons.coffee_outlined,
-  'bakery_dining': Icons.bakery_dining_outlined,
-  'set_meal': Icons.set_meal_outlined,
-  'egg': Icons.egg_outlined,
-  'rice_bowl': Icons.rice_bowl_outlined,
-  'emoji_food_beverage': Icons.emoji_food_beverage_outlined,
-  'shopping_cart': Icons.shopping_cart_outlined,
+  'inventory_2': LucideIcons.package,
+  'icecream': LucideIcons.iceCream,
+  'local_pizza': LucideIcons.pizza,
+  'lunch_dining': LucideIcons.utensils,
+  'local_drink': LucideIcons.cupSoda,
+  'water_drop': LucideIcons.droplets,
+  'breakfast_dining': LucideIcons.egg,
+  'donut_small': LucideIcons.donut,
+  'cookie': LucideIcons.cookie,
+  'apple': LucideIcons.apple,
+  'fastfood': LucideIcons.sandwich,
+  'cake': LucideIcons.cake,
+  'coffee': LucideIcons.coffee,
+  'bakery_dining': LucideIcons.croissant,
+  'set_meal': LucideIcons.beef,
+  'egg': LucideIcons.egg,
+  'rice_bowl': LucideIcons.soup,
+  'emoji_food_beverage': LucideIcons.coffee,
+  'shopping_cart': LucideIcons.shoppingCart,
 };
 
 const Map<String, String> categoryIcons = {
@@ -42,5 +43,5 @@ IconData resolveProductIcon(Map<String, dynamic> product) {
   if (catIcon != null && productIcons.containsKey(catIcon)) {
     return productIcons[catIcon]!;
   }
-  return Icons.shopping_bag;
+  return LucideIcons.shoppingBag;
 }
